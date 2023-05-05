f2302f5fb9a04dd70bb2d1c607ce8ddc9e0e425c
Theophile Gervet <theophile.gervet@gmail.com>
2023-05-05 09:42:12 -0400

    256x256 feature map

--- scripts/launch_train_fair.sh
+++ scripts/launch_train_fair.sh
@@ -21,7 +21,34 @@
 #   --run_log_dir $task-PERACT
 #done
 
-main_dir=05_05_eval_6d_rotation
+#main_dir=05_05_eval_6d_rotation
+#use_instruction=0
+#task_file=tasks/hiveformer_high_precision_tasks.csv
+#gripper_loc_bounds_file=tasks/74_hiveformer_tasks_location_bounds.json
+#dataset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_train
+#valset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_val
+#train_iters=400_000
+##for task in $(cat $task_file | tr '\n' ' '); do
+#for task in insert_onto_square_peg place_shape_in_shape_sorter; do
+#  for rotation_parametrization in 6D_from_query; do
+#    for rotation_loss_coeff in 1.0 0.1; do
+#      sbatch train_1gpu_32gb_fair.sh \
+#       --tasks $task \
+#       --dataset $dataset \
+#       --valset $valset \
+#       --exp_log_dir $main_dir \
+#       --gripper_loc_bounds_file $gripper_loc_bounds_file \
+#       --use_instruction $use_instruction \
+#       --logger wandb \
+#       --train_iters $train_iters \
+#       --rotation_parametrization $rotation_parametrization \
+#       --rotation_loss_coeff $rotation_loss_coeff \
+#       --run_log_dir $task-HIVEFORMER-$rotation_parametrization-$rotation_loss_coeff
+#    done
+#  done
+#done
+
+main_dir=05_05_eval_256x256_feature_map
 use_instruction=0
 task_file=tasks/hiveformer_high_precision_tasks.csv
 gripper_loc_bounds_file=tasks/74_hiveformer_tasks_location_bounds.json
@@ -29,21 +56,15 @@ dataset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_tra
 valset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_val
 train_iters=400_000
 #for task in $(cat $task_file | tr '\n' ' '); do
-for task in insert_onto_square_peg place_shape_in_shape_sorter; do
-  for rotation_parametrization in 6D_from_query; do
-    for rotation_loss_coeff in 1.0 0.1; do
-      sbatch train_1gpu_32gb_fair.sh \
-       --tasks $task \
-       --dataset $dataset \
-       --valset $valset \
-       --exp_log_dir $main_dir \
-       --gripper_loc_bounds_file $gripper_loc_bounds_file \
-       --use_instruction $use_instruction \
-       --logger wandb \
-       --train_iters $train_iters \
-       --rotation_parametrization $rotation_parametrization \
-       --rotation_loss_coeff $rotation_loss_coeff \
-       --run_log_dir $task-HIVEFORMER-$rotation_parametrization-$rotation_loss_coeff
-    done
-  done
+for task in place_shape_in_shape_sorter screw_nail sweep_to_dustpan; do
+    sbatch train_1gpu_32gb_fair.sh \
+     --tasks $task \
+     --dataset $dataset \
+     --valset $valset \
+     --exp_log_dir $main_dir \
+     --gripper_loc_bounds_file $gripper_loc_bounds_file \
+     --use_instruction $use_instruction \
+     --logger wandb \
+     --train_iters $train_iters \
+     --run_log_dir $task-HIVEFORMER-256x256-feature-map
 done
