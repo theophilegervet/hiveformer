eb19e19bc307c9baeb82d6a7256cf6f9d77f4f16
Theo Gervet <theop123@meta.com>
2023-05-11 17:09:37 -0400

    optimize data storage

--- data_preprocessing/data_gen.py
+++ data_preprocessing/data_gen.py
@@ -164,8 +164,7 @@ class Dataset(torch.utils.data.Dataset):
 
 if __name__ == "__main__":
     args = Arguments().parse_args()
-    print(args.cameras)
-    print(args.tasks)
+    args.cameras = [x for x in [y.split(",") for y in args.cameras]]
 
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
