673f538ddbd73e12aecfbf4bbc2c69948e81d344
Theo Gervet <theop123@meta.com>
2023-05-12 18:42:00 -0400

    Train 10 ep

--- scripts/launch_train_fair.sh
+++ scripts/launch_train_fair.sh
@@ -1,21 +1,38 @@
 #!/bin/sh
 
-main_dir=peract
-use_instruction=1
-task_file=tasks/peract_18_tasks.csv
-gripper_loc_bounds_file=tasks/18_peract_tasks_location_bounds.json
-dataset=/private/home/theop123/datasets/rlbench/packaged/18_peract_tasks_train
-valset=/private/home/theop123/datasets/rlbench/packaged/18_peract_tasks_val
+#main_dir=peract
+#use_instruction=1
+#task_file=tasks/peract_18_tasks.csv
+#gripper_loc_bounds_file=tasks/18_peract_tasks_location_bounds.json
+#dataset=/private/home/theop123/datasets/rlbench/packaged/18_peract_tasks_train
+#valset=/private/home/theop123/datasets/rlbench/packaged/18_peract_tasks_val
 
 #main_dir=hiveformer
-#use_instruction=0
-#task_file=tasks/hiveformer_74_tasks.csv
-#gripper_loc_bounds_file=tasks/74_hiveformer_tasks_location_bounds.json
-#dataset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_train
-#valset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_val
+use_instruction=0
+task_file=tasks/hiveformer_74_tasks.csv
+gripper_loc_bounds_file=tasks/74_hiveformer_tasks_location_bounds.json
+dataset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_train
+valset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_val
 
-# Single-task
+# Single-task PerAct
+#train_iters=400_000
+#for task in $(cat $task_file | tr '\n' ' '); do
+#  sbatch train_1gpu_32gb_fair.sh \
+#   --tasks task \
+#   --dataset $dataset \
+#   --valset $valset \
+#   --exp_log_dir $main_dir \
+#   --gripper_loc_bounds_file $gripper_loc_bounds_file \
+#   --use_instruction $use_instruction \
+#   --logger wandb \
+#   --train_iters $train_iters \
+#   --variations {0..199} \
+#   --run_log_dir $task-PERACT
+#done
+
+# Single-task HiveFormer
 train_iters=400_000
+max_episodes_per_task=10
 for task in $(cat $task_file | tr '\n' ' '); do
   sbatch train_1gpu_32gb_fair.sh \
    --tasks task \
@@ -26,26 +43,26 @@ for task in $(cat $task_file | tr '\n' ' '); do
    --use_instruction $use_instruction \
    --logger wandb \
    --train_iters $train_iters \
-   --variations {0..199} \
-   --run_log_dir $task-PERACT
+   --max_episodes_per_task $max_episodes_per_task \
+   --run_log_dir $task-HIVEFORMER-10-episodes
 done
 
-# Multi-task
-train_iters=4_000_000
-sbatch train_1gpu_32gb_fair.sh \
-   --devices cuda:0 cuda:1 cuda:2 cuda:3 \
-   --tasks $(cat $task_file | tr '\n' ' ') \
-   --batch_size 32 \
-   --batch_size_val 8 \
-   --num_workers 16 \
-   --cache_size 0 \
-   --cache_size_val 0 \
-   --dataset $dataset \
-   --valset $valset \
-   --exp_log_dir $main_dir \
-   --gripper_loc_bounds_file $gripper_loc_bounds_file \
-   --use_instruction $use_instruction \
-   --logger wandb \
-   --train_iters $train_iters \
-   --variations {0..199} \
-   --run_log_dir MULTI-TASK-PERACT
+# Multi-task PerAct
+#train_iters=4_000_000
+#sbatch train_1gpu_32gb_fair.sh \
+#   --devices cuda:0 cuda:1 cuda:2 cuda:3 \
+#   --tasks $(cat $task_file | tr '\n' ' ') \
+#   --batch_size 32 \
+#   --batch_size_val 8 \
+#   --num_workers 16 \
+#   --cache_size 0 \
+#   --cache_size_val 0 \
+#   --dataset $dataset \
+#   --valset $valset \
+#   --exp_log_dir $main_dir \
+#   --gripper_loc_bounds_file $gripper_loc_bounds_file \
+#   --use_instruction $use_instruction \
+#   --logger wandb \
+#   --train_iters $train_iters \
+#   --variations {0..199} \
+#   --run_log_dir MULTI-TASK-PERACT
