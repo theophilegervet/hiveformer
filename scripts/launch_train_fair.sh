5c7dcd7024619ea903ad4887132c00b238597389
Theo Gervet <theop123@meta.com>
2023-05-12 21:43:01 -0400

    Train 10 ep

--- scripts/launch_train_fair.sh
+++ scripts/launch_train_fair.sh
@@ -31,22 +31,22 @@ valset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_val
 #done
 
 # Single-task HiveFormer
-main_dir=hiveformer_10_episodes
-train_iters=400_000
-max_episodes_per_task=10
-for task in $(cat $task_file | tr '\n' ' '); do
-  sbatch train_1gpu_32gb_fair.sh \
-   --tasks task \
-   --dataset $dataset \
-   --valset $valset \
-   --exp_log_dir $main_dir \
-   --gripper_loc_bounds_file $gripper_loc_bounds_file \
-   --use_instruction $use_instruction \
-   --logger wandb \
-   --train_iters $train_iters \
-   --max_episodes_per_task $max_episodes_per_task \
-   --run_log_dir $task-HIVEFORMER-10-episodes
-done
+#main_dir=hiveformer_10_episodes
+#train_iters=400_000
+#max_episodes_per_task=10
+#for task in $(cat $task_file | tr '\n' ' '); do
+#  sbatch train_1gpu_32gb_fair.sh \
+#   --tasks $task \
+#   --dataset $dataset \
+#   --valset $valset \
+#   --exp_log_dir $main_dir \
+#   --gripper_loc_bounds_file $gripper_loc_bounds_file \
+#   --use_instruction $use_instruction \
+#   --logger wandb \
+#   --train_iters $train_iters \
+#   --max_episodes_per_task $max_episodes_per_task \
+#   --run_log_dir $task-HIVEFORMER-10-episodes
+#done
 
 # Multi-task PerAct
 #train_iters=4_000_000
