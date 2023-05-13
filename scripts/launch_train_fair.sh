0f51c8c9cdf104bb2c29ce693a1e47593dbb7055
Theo Gervet <theop123@meta.com>
2023-05-12 22:59:28 -0400

    launch

--- scripts/launch_train_fair.sh
+++ scripts/launch_train_fair.sh
@@ -1,61 +1,73 @@
 #!/bin/sh
 
-#main_dir=05_04_eval_on_peract_18_tasks
-#use_instruction=1
-#task_file=tasks/peract_18_tasks.csv
-#gripper_loc_bounds_file=tasks/18_peract_tasks_location_bounds.json
-#dataset=/private/home/theop123/datasets/rlbench/packaged/18_peract_tasks_train
-#valset=/private/home/theop123/datasets/rlbench/packaged/18_peract_tasks_val
-main_dir=05_08_eval_on_hiveformer_74_tasks
-use_instruction=0
-task_file=tasks/hiveformer_74_tasks.csv
-gripper_loc_bounds_file=tasks/74_hiveformer_tasks_location_bounds.json
-dataset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_train
-valset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_val
+main_dir=peract_new_data
+use_instruction=1
+cameras=left_shoulder,right_shoulder,wrist,front
+task_file=tasks/peract_18_tasks.csv
+gripper_loc_bounds_file=tasks/18_peract_tasks_location_bounds.json
+dataset=/private/home/theop123/datasets/rlbench/packaged/18_peract_tasks_train_new
+valset=/private/home/theop123/datasets/rlbench/packaged/18_peract_tasks_val_new
+
+#main_dir=hiveformer
+#use_instruction=0
+#task_file=tasks/hiveformer_74_tasks.csv
+#gripper_loc_bounds_file=tasks/74_hiveformer_tasks_location_bounds.json
+#dataset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_train
+#valset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_val
+
+# Single-task PerAct
 train_iters=400_000
+for task in $(cat $task_file | tr '\n' ' '); do
+  sbatch train_1gpu_32gb_128gb_fair.sh \
+   --tasks task \
+   --cameras $cameras \
+   --dataset $dataset \
+   --valset $valset \
+   --cache_size_val 0 \
+   --exp_log_dir $main_dir \
+   --gripper_loc_bounds_file $gripper_loc_bounds_file \
+   --use_instruction $use_instruction \
+   --logger wandb \
+   --train_iters $train_iters \
+   --variations {0..199} \
+   --run_log_dir $task-PERACT
+done
+
+# Single-task HiveFormer
+#main_dir=hiveformer_10_episodes
+#train_iters=400_000
+#max_episodes_per_task=10
 #for task in $(cat $task_file | tr '\n' ' '); do
-for task in take_shoes_out_of_box; do
-  sbatch train_1gpu_32gb_256gb_fair.sh \
-   --tasks $task \
+#  sbatch train_1gpu_32gb_fair.sh \
+#   --tasks $task \
+#   --dataset $dataset \
+#   --valset $valset \
+#   --exp_log_dir $main_dir \
+#   --gripper_loc_bounds_file $gripper_loc_bounds_file \
+#   --use_instruction $use_instruction \
+#   --logger wandb \
+#   --train_iters $train_iters \
+#   --max_episodes_per_task $max_episodes_per_task \
+#   --run_log_dir $task-HIVEFORMER-10-episodes
+#done
+
+# Multi-task PerAct
+train_iters=4_000_000
+sbatch train_4gpu_32gb_fair.sh \
+   --devices cuda:0 cuda:1 cuda:2 cuda:3 \
+   --tasks $(cat $task_file | tr '\n' ' ') \
+   --cameras $cameras \
+   --batch_size 32 \
+   --batch_size_val 8 \
+   --num_workers 16 \
+   --cache_size 0 \
+   --cache_size_val 0 \
    --dataset $dataset \
    --valset $valset \
    --exp_log_dir $main_dir \
    --gripper_loc_bounds_file $gripper_loc_bounds_file \
    --use_instruction $use_instruction \
    --logger wandb \
-   --batch_size 8 \
-   --batch_size_val 2 \
    --train_iters $train_iters \
-   --run_log_dir $task-HIVEFORMER \
-   --cache_size_val 0
-   #   --variations {0..199} \
-done
-
-#main_dir=05_05_eval_6d_rotation
-#use_instruction=0
-#task_file=tasks/hiveformer_high_precision_tasks.csv
-#gripper_loc_bounds_file=tasks/74_hiveformer_tasks_location_bounds.json
-#dataset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_train
-#valset=/private/home/theop123/datasets/rlbench/packaged/74_hiveformer_tasks_val
-#train_iters=400_000
-##for task in $(cat $task_file | tr '\n' ' '); do
-#for task in insert_onto_square_peg place_shape_in_shape_sorter; do
-#  for rotation_parametrization in quat_from_query 6D_from_query; do
-#    for rotation_loss_coeff in 100.0; do
-#      sbatch train_1gpu_32gb_fair.sh \
-#       --tasks $task \
-#       --dataset $dataset \
-#       --valset $valset \
-#       --exp_log_dir $main_dir \
-#       --gripper_loc_bounds_file $gripper_loc_bounds_file \
-#       --use_instruction $use_instruction \
-#       --logger wandb \
-#       --train_iters $train_iters \
-#       --rotation_parametrization $rotation_parametrization \
-#       --rotation_loss_coeff $rotation_loss_coeff \
-#       --batch_size 8 \
-#       --batch_size_val 2 \
-#       --run_log_dir $task-HIVEFORMER-$rotation_parametrization-$rotation_loss_coeff
-#    done
-#  done
-#done
+   --variations {0..199} \
+   --run_log_dir MULTI-TASK-PERACT
