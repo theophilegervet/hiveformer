b648196be3e2c8691de0a92cbb41b875dfe266e6
Theo Gervet <theop123@meta.com>
2023-05-16 17:51:40 -0400

    bug in checkpointing?

--- scripts/launch_train_fair.sh
+++ scripts/launch_train_fair.sh
@@ -63,10 +63,12 @@ train_iters=4_000_000
 batch_size=24
 batch_size_val=4
 num_workers=24
+checkpoint="/private/home/theop123/hiveformer2/train_logs/peract_new_data/MULTI-TASK-PERACT_version8002386/best.pth"
 for embedding_dim in 120; do
   sbatch train_4gpu_32gb_fair.sh \
      --devices cuda:0 cuda:1 cuda:2 cuda:3 \
      --tasks $(cat $task_file | tr '\n' ' ') \
+     --checkpoint $checkpoint \
      --cameras $cameras \
      --embedding_dim $embedding_dim \
      --batch_size $batch_size \
