88d07af8d2a4aefc6f95c9860de9b54552320f2d
Theo Gervet <theop123@meta.com>
2023-05-16 17:32:37 -0400

    debug

--- train.py
+++ train.py
@@ -205,7 +205,7 @@ def training(
             train_losses["total"] = sum(list(train_losses.values()))  # type: ignore
             train_losses["total"].backward()  # type: ignore
 
-            print("GRAD", model.module.prediction_head.query_cross_attn_pyramid.ffw_layers[0].weight.grad)
+            print("GRAD", model.module.prediction_head.query_cross_attn_pyramid[0].ffw_layers[0].weight.grad)
             raise NotImplementedError
 
             metrics = loss_and_metrics.compute_metrics(pred, sample)
