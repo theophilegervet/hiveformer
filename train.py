1c36947abe5a5c6b88aa6192ad632b60c59bdb7e
Theo Gervet <theop123@meta.com>
2023-05-17 22:33:25 -0400

    debug

--- train.py
+++ train.py
@@ -278,7 +278,7 @@ def training(
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
-    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
 
 def get_log_dir(args: Arguments) -> Path:
