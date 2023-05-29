254214ac2f80f4759bd533da6d2038ab35c25cb7
Theo Gervet <theop123@meta.com>
2023-05-29 10:01:56 -0400

    Eval HiveFormer 10 eps

--- scripts/launch_eval_local.sh
+++ scripts/launch_eval_local.sh
@@ -1,84 +1,155 @@
-# HIVEFORMER
-exp=03_24_hiveformer_setting
+exp=hiveformer_10_episodes
 ckpts=(
-#  open_fridge-HIVEFORMER_version161072
-#  hang_frame_on_hanger-HIVEFORMER_version161481
-#  open_door-HIVEFORMER_version161482
-#  take_frame_off_hanger-HIVEFORMER_version161654
-#  open_oven-HIVEFORMER_version161968
-#  put_books_on_bookshelf-HIVEFORMER_version161973
-#  straighten_rope-HIVEFORMER_version163672
-#  change_channel-HIVEFORMER_version165805
-#  tv_on-HIVEFORMER_version164906
-#  slide_cabinet_open_and_place_cups-HIVEFORMER_version165804
-  stack_cups-HIVEFORMER_version7658919
-  stack_blocks-HIVEFORMER_version7658922
-)
-tasks=(
-#  open_fridge
-#  hang_frame_on_hanger
-#  open_door
-#  take_frame_off_hanger
-#  open_oven
-#  put_books_on_bookshelf
-#  straighten_rope
-#  change_channel
-#  tv_on
-#  slide_cabinet_open_and_place_cups
-  stack_cups
-  stack_blocks
+  reach_target-HIVEFORMER-10-episodes_version8815937
+  lamp_off-HIVEFORMER-10-episodes_version8815940
+  close_fridge-HIVEFORMER-10-episodes_version8815938
+  push_button-HIVEFORMER-10-episodes_version8815942
+  close_microwave-HIVEFORMER-10-episodes_version8815939
+  press_switch-HIVEFORMER-10-episodes_version8815941
+  unplug_charger-HIVEFORMER-10-episodes_version8815946
+  close_drawer-HIVEFORMER-10-episodes_version8815937
+  play_jenga-HIVEFORMER-10-episodes_version8815958
+  slide_cabinet_open_and_place_cups-HIVEFORMER-10-episodes_version8816006
+  insert_onto_square_peg-HIVEFORMER-10-episodes_version8815982
+  toilet_seat_up-HIVEFORMER-10-episodes_version8815960
+  stack_wine-HIVEFORMER-10-episodes_version8816001
+  pick_and_lift_small-HIVEFORMER-10-episodes_version8815971
+  phone_on_base-HIVEFORMER-10-episodes_version8815987
+  put_money_in_safe-HIVEFORMER-10-episodes_version8815992
+  open_microwave-HIVEFORMER-10-episodes_version8815955
+  place_shape_in_shape_sorter-HIVEFORMER-10-episodes_version8815989
+  wipe_desk-HIVEFORMER-10-episodes_version8816003
+  setup_checkers-HIVEFORMER-10-episodes_version8816000
+  meat_off_grill-HIVEFORMER-10-episodes_version8815984
+  open_box-HIVEFORMER-10-episodes_version8815950
+  slide_block_to_target-HIVEFORMER-10-episodes_version8815944
+  move_hanger-HIVEFORMER-10-episodes_version8815985
+  open_drawer-HIVEFORMER-10-episodes_version8815952
+  straighten_rope-HIVEFORMER-10-episodes_version8816004
+  take_usb_out_of_computer-HIVEFORMER-10-episodes_version8815945
+  sweep_to_dustpan-HIVEFORMER-10-episodes_version8815993
+  insert_usb_in_computer-HIVEFORMER-10-episodes_version8815984
+  lamp_on-HIVEFORMER-10-episodes_version8815948
+  stack_blocks-HIVEFORMER-10-episodes_version8816009
+  close_grill-HIVEFORMER-10-episodes_version8815966
+  take_frame_off_hanger-HIVEFORMER-10-episodes_version8815976
+  put_umbrella_in_umbrella_stand-HIVEFORMER-10-episodes_version8815974
+  put_knife_on_chopping_board-HIVEFORMER-10-episodes_version8815972
+  open_window-HIVEFORMER-10-episodes_version8815969","running
+  take_umbrella_out_of_umbrella_stand-HIVEFORMER-10-episodes_version8815959
+  change_clock-HIVEFORMER-10-episodes_version8815964
+  pick_up_cup-HIVEFORMER-10-episodes_version8815957
+  place_hanger_on_rack-HIVEFORMER-10-episodes_version8815988
+  hang_frame_on_hanger-HIVEFORMER-10-episodes_version8815968
+  turn_oven_on-HIVEFORMER-10-episodes_version8815961
+  pick_and_lift-HIVEFORMER-10-episodes_version8815970
+  lift_numbered_block-HIVEFORMER-10-episodes_version8815949
+  open_wine_bottle-HIVEFORMER-10-episodes_version8815956
+  put_books_on_bookshelf-HIVEFORMER-10-episodes_version8815991
+  take_money_out_safe-HIVEFORMER-10-episodes_version8815977
+  close_door-HIVEFORMER-10-episodes_version8815947
+  stack_cups-HIVEFORMER-10-episodes_version8816007
+  basketball_in_hoop-HIVEFORMER-10-episodes_version8815962
+  reach_and_drag-HIVEFORMER-10-episodes_version8815997
+  push_buttons-HIVEFORMER-10-episodes_version8815996
+  tv_on-HIVEFORMER-10-episodes_version8816005
+  take_lid_off_saucepan-HIVEFORMER-10-episodes_version8815959
+  plug_charger_in_power_supply-HIVEFORMER-10-episodes_version8815990
+  change_channel-HIVEFORMER-10-episodes_version8816004
+  put_rubbish_in_bin-HIVEFORMER-10-episodes_version8815973
+  open_fridge-HIVEFORMER-10-episodes_version8815953
+  take_toilet_roll_off_stand-HIVEFORMER-10-episodes_version8815979
+  open_grill-HIVEFORMER-10-episodes_version8815954
+  open_door-HIVEFORMER-10-episodes_version8815968
+  toilet_seat_down-HIVEFORMER-10-episodes_version8815980
+  meat_on_grill-HIVEFORMER-10-episodes_version8815987
+  open_oven-HIVEFORMER-10-episodes_version8815986
+  screw_nail-HIVEFORMER-10-episodes_version8815998
+  take_plate_off_colored_dish_rack-HIVEFORMER-10-episodes_version8815994
+  tower3-HIVEFORMER-10-episodes_version8816002
+  water_plants-HIVEFORMER-10-episodes_version8815995
+  close_box-HIVEFORMER-10-episodes_version8815981
 )
 
-# PERACT
-exp=05_04_eval_on_peract_18_tasks
-ckpts=(
-  light_bulb_in-PERACT_version7598189
-  stack_cups-PERACT_version7642636
-  place_cups-PERACT_version7642637
-  put_item_in_drawer-PERACT_version7642638
-  stack_blocks-PERACT_version7642639
-#  turn_tap-PERACT_version7453200
-#  open_drawer-PERACT_version7453201
-#  push_buttons-PERACT_version7453202
-#  sweep_to_dustpan_of_size-PERACT_version7453203
-#  slide_block_to_color_target-PERACT_version7453204
-#  meat_off_grill-PERACT_version7453206
-#  put_groceries_in_cupboard-PERACT_version7453209
-#  close_jar-PERACT_version7453211
-#  place_shape_in_shape_sorter-PERACT_version7598184
-#  place_wine_at_rack_location-PERACT_version7598185
-#  insert_onto_square_peg-PERACT_version7598186
-#  put_money_in_safe-PERACT_version7598187
-#  reach_and_drag-PERACT_version7598188
-)
 tasks=(
-  light_bulb_in
-  stack_cups
-  place_cups
-  put_item_in_drawer
+  reach_target
+  lamp_off
+  close_fridge
+  push_button
+  close_microwave
+  press_switch
+  unplug_charger
+  close_drawer
+  play_jenga
+  slide_cabinet_open_and_place_cups
+  insert_onto_square_peg
+  toilet_seat_up
+  stack_wine
+  pick_and_lift_small
+  phone_on_base
+  put_money_in_safe
+  open_microwave
+  place_shape_in_shape_sorter
+  wipe_desk
+  setup_checkers
+  meat_off_grill
+  open_box
+  slide_block_to_target
+  move_hanger
+  open_drawer
+  straighten_rope
+  take_usb_out_of_computer
+  sweep_to_dustpan
+  insert_usb_in_computer
+  lamp_on
   stack_blocks
-#  turn_tap
-#  open_drawer
-#  push_buttons
-#  sweep_to_dustpan_of_size
-#  slide_block_to_color_target
-#  meat_off_grill
-#  put_groceries_in_cupboard
-#  close_jar
-#  place_shape_in_shape_sorter
-#  place_wine_at_rack_location
-#  insert_onto_square_peg
-#  put_money_in_safe
-#  reach_and_drag
+  close_grill
+  take_frame_off_hanger
+  put_umbrella_in_umbrella_stand
+  put_knife_on_chopping_board
+  open_window
+  take_umbrella_out_of_umbrella_stand
+  change_clock
+  pick_up_cup
+  place_hanger_on_rack
+  hang_frame_on_hanger
+  turn_oven_on
+  pick_and_lift
+  lift_numbered_block
+  open_wine_bottle
+  put_books_on_bookshelf
+  take_money_out_safe
+  close_door
+  stack_cups
+  basketball_in_hoop
+  reach_and_drag
+  push_buttons
+  tv_on
+  take_lid_off_saucepan
+  plug_charger_in_power_supply
+  change_channel
+  put_rubbish_in_bin
+  open_fridge
+  take_toilet_roll_off_stand
+  open_grill
+  open_door
+  toilet_seat_down
+  meat_on_grill
+  open_oven
+  screw_nail
+  take_plate_off_colored_dish_rack
+  tower3
+  water_plants
+  close_box
 )
 
-#data_dir=/home/zhouxian/git/datasets/raw/74_hiveformer_tasks_val
-data_dir=/home/zhouxian/git/datasets/raw/18_peract_tasks_val
-num_episodes=100
-#gripper_loc_bounds_file=tasks/74_hiveformer_tasks_location_bounds.json
-gripper_loc_bounds_file=tasks/18_peract_tasks_location_bounds.json
-#use_instruction=0
-use_instruction=1
+data_dir=/home/zhouxian/git/datasets/raw/74_hiveformer_tasks_val
+#data_dir=/home/zhouxian/git/datasets/raw/18_peract_tasks_val
+num_episodes=50
+gripper_loc_bounds_file=tasks/74_hiveformer_tasks_location_bounds.json
+#gripper_loc_bounds_file=tasks/18_peract_tasks_location_bounds.json
+use_instruction=0
+#use_instruction=1
 num_ghost_points=10000
 headless=1
 offline=0
@@ -88,8 +159,8 @@ for ((i=0; i<$num_ckpts; i++)); do
   python eval.py --tasks ${tasks[$i]} --checkpoint $exp/${ckpts[$i]}/best.pth \
     --data_dir $data_dir --offline $offline --num_episodes $num_episodes --headless $headless --output_file eval/${tasks[$i]}.json  \
     --exp_log_dir $exp --run_log_dir ${tasks[$i]}-ONLINE --record_videos 0 --use_instruction $use_instruction \
-    --gripper_loc_bounds_file $gripper_loc_bounds_file --num_ghost_points $num_ghost_points --num_ghost_points_val $num_ghost_points \
-    --variations {0..60}
+    --gripper_loc_bounds_file $gripper_loc_bounds_file --num_ghost_points $num_ghost_points --num_ghost_points_val $num_ghost_points
+#    --variations {0..60}
 done
 
 
