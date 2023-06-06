00bee89fbd820a20c7090af1f9e79e9d85ee6a19
Theo Gervet <theop123@meta.com>
2023-06-06 19:44:22 -0400

    Record videos

--- scripts/launch_eval_local.sh
+++ scripts/launch_eval_local.sh
@@ -37,18 +37,12 @@ ckpts=(
   multi-task2
   multi-task2
   multi-task2
-  multi-task2
-  multi-task2
-  multi-task2
 )
 tasks=(
-  turn_tap
-  insert_onto_square_peg
-  place_shape_in_shape_sorter
   put_groceries_in_cupboard
+  place_cups
   light_bulb_in
   stack_cups
-  place_cups
   stack_blocks
 )
 
@@ -57,11 +51,12 @@ tasks=(
 #data_dir=/home/sirdome/katefgroup/datasets/raw/18_peract_tasks_val_new
 data_dir=/home/katefgroup/Documents/datasets/rlbench/raw/18_peract_tasks_val_new
 #data_dir=/home/katefgroup/Documents/datasets/rlbench/raw/74_hiveformer_tasks_val
-num_episodes=100
+num_episodes=50
 #gripper_loc_bounds_file=tasks/74_hiveformer_tasks_location_bounds.json
 gripper_loc_bounds_file=tasks/18_peract_tasks_location_bounds.json
 #use_instruction=0
 use_instruction=1
+collision_checking=1
 num_ghost_points=10000
 headless=0
 offline=0
@@ -75,7 +70,7 @@ for ((i=0; i<$num_ckpts; i++)); do
     --data_dir $data_dir --offline $offline --num_episodes $num_episodes --headless $headless --output_file eval/${tasks[$i]}.json  \
     --exp_log_dir $exp --run_log_dir ${tasks[$i]}-ONLINE --record_videos $record_videos --use_instruction $use_instruction \
     --gripper_loc_bounds_file $gripper_loc_bounds_file --num_ghost_points $num_ghost_points --num_ghost_points_val $num_ghost_points \
-    --variations {0..60} --cameras $cameras --embedding_dim $embedding_dim
+    --variations {0..60} --cameras $cameras --embedding_dim $embedding_dim --collision_checking $collision_checking
 done
 
 
