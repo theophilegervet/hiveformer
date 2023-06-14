bf75187c03be6087ea90fa3dd896932202b888f7
Theo Gervet <theop123@meta.com>
2023-06-13 18:03:04 -0700

    Visualizations

--- utils/utils_with_rlbench.py
+++ utils/utils_with_rlbench.py
@@ -610,7 +610,7 @@ class RLBenchEnv:
                         pcd_obs = np.stack([getattr(obs, f"{cam}_point_cloud") for cam in cameras])
                         rgb_obs = einops.rearrange(rgb_obs[:, :, :, :3], "n_cam h w c -> (n_cam h w) c")
                         rgb_obs = rgb_obs / 255.0
-                        # rgb_obs = 2 * (rgb_obs - 0.5)
+                        rgb_obs = 2 * (rgb_obs - 0.5)
                         print(rgb_obs.max(), rgb_obs.min())
                         pcd_obs = einops.rearrange(pcd_obs, "n_cam h w c -> (n_cam h w) c")
                         opcd = open3d.geometry.PointCloud()
