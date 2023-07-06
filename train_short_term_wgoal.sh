e9e59b78dc95283d7974931edf112762403782b2
theop123 <act3d.act3d@gmail.com>
2023-07-05 21:45:51 -0700

    Launch scripts

--- train_short_term_wgoal.sh
+++ train_short_term_wgoal.sh
@@ -1,24 +1,22 @@
-dataset=/projects/katefgroup/datasets/rlbench/diffusion_trajectories_train/
-valset=/projects/katefgroup/datasets/rlbench/diffusion_trajectories_val/
-
-main_dir=shortterm_07_01_multitask_2gpus
-
-
-# task=unplug_charger close_door open_box open_fridge put_umbrella_in_umbrella_stand take_frame_off_hanger open_oven put_books_on_bookshelf slide_cabinet_open_and_place_cups wipe_desk
+dataset=/private/home/theop123/datasets/rlbench/packaged/diffusion_trajectories_train/
+valset=/private/home/theop123/datasets/rlbench/packaged/diffusion_trajectories_val/
+instructions=/private/home/theop123/hiveformer/instructions.pkl
+main_dir=diffusion_short_term_wgoal
 lr=1e-4
 dense_interpolation=1
 interpolation_length=50
-B=24
-ngpus=1
+B=3
+ngpus=8
 
-CUDA_LAUNCH_BLOCKING=1 python -m torch.distributed.launch --nproc_per_node $ngpus --master_port $RANDOM \
+#CUDA_LAUNCH_BLOCKING=1 python -m torch.distributed.launch --nproc_per_node $ngpus --master_port $RANDOM \
+sbatch train_8gpu_32gb_fair_devlab.sh \
     main_trajectory.py --tasks unplug_charger close_door open_box open_fridge put_umbrella_in_umbrella_stand take_frame_off_hanger open_oven put_books_on_bookshelf slide_cabinet_open_and_place_cups wipe_desk \
     --dataset  $dataset\
     --valset $valset \
-    --instructions /home/tgervet/hiveformer/instructions.pkl \
+    --instructions $instructions \
     --gripper_loc_bounds 10_tough_diffusion_location_bounds.json \
     --num_workers 4\
-    --train_iters 500000 \
+    --train_iters 1000000 \
     --model diffusion \
     --num_query_cross_attn_layers 2 \
     --feat_scales_to_use 3 \
@@ -36,4 +34,4 @@ CUDA_LAUNCH_BLOCKING=1 python -m torch.distributed.launch --nproc_per_node $ngpu
     --cache_size 0 \
     --cache_size_val 0 \
     --lr $lr\
-    --run_log_dir multi-B$B-lr$lr-short_term
+    --run_log_dir short_term_wgoal-B$B-lr$lr-short_term
